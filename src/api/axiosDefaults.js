12f1e6de5f5985a61f5b2264f39124595dc214aa
Kattis91 <katya.svedmark@gmail.com>
2023-10-31 00:24:21 +0000

    Create a Sign Up fomr

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://my-django-rest-framework-196a9d11f298.herokuapp.com/'
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+axios.defaults.withCredentials = true
