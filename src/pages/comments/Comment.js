944a3522593bad867432145f16d9ee8350af833b
Kattis91 <katya.svedmark@gmail.com>
2023-11-01 20:20:49 +0000

    Add comment component

---
+++ src/pages/comments/Comment.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Media } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Avatar from "../../components/Avatar";
+import styles from "../../styles/Comment.module.css";
+
+const Comment = (props) => {
+  const { profile_id, profile_image, owner, updated_at, content } = props;
+
+  return (
+    <div>
+      <hr />
+      <Media>
+        <Link to={`/profiles/${profile_id}`}>
+          <Avatar src={profile_image} />
+        </Link>
+          <span className={styles.Owner}>{owner}</span>
+          <span className={styles.Date}>{updated_at}</span>
+          <p className={styles.Content}>{content}</p>
+      </Media>
+    </div>
+  );
+};
+
+export default Comment;
